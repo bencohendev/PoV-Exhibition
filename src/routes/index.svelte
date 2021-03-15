440890008351bd9511e6212c94aa92872e1b9fc2
bencohendev <info@bencohen.dev>
2021-03-15 16:35:27 +0000

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>PoV Exhibition</title>
+</svelte:head>
+
+<h1>Choose an artwork</h1>
+
+<style lang="scss">
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
